81222c573e1e18caa092b3b0119bb71c1af130c3
Lorenz Koestler (lko) <lorenz.koestler@datahouse.ch>
2017-05-01 12:21:52 +0200

    remove koestler specific build scripts; datahouse build scripts are held in git.datalan.ch:it-docker-dnsdock

--- docker_dev.sh
+++ docker_dev.sh
@@ -1,6 +1,6 @@
 docker run --rm -it \
-       -v `pwd`:/go/src/github.com/koestler/dnsdock \
+       -v `pwd`:/go/src/github.com/datahouse/dnsdock \
        -v /var/run/docker.sock:/var/run/docker.sock \
        -v /etc/NetworkManager/dnsmasq.d/:/etc/dnsmasq.d/ \
        golang:1.8-wheezy bash
-# now run cd src/github.com/koestler/dnsdock && ./build.sh
+# now run cd src/github.com/datahouse/dnsdock
